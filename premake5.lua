a92121d29860529affbc72612ab99eaa28004758
Thomas Payne <tommypayne093@gmail.com>
2025-07-02 09:37:57 -0400

    Added premake5

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "RTXMU"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "include/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "on"
+        optimize "off"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "Speed"
+        symbols "off"
+
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "Speed"
+        symbols "off"
